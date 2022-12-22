70e88a253f1e13806ad4ad52a2f735783a1262a8
Michael Weigelt <michael.weigelt@bluewin.ch>
2022-12-22 22:27:41 +0100

    added floor, blocking wall

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 
 Roughly following [this tutorial](https://tomassedovic.github.io/roguelike-tutorial/index.html).
 
+
+
+### gameplay goals
+* encounter injured creatures and heal them
+* plant plants, water them, watch them grow
+* fix bridges, streets, waterwheels, buildings
+* bring messages to other dungeon levels, specific creatures
+* find treasure, gift it to creatures or use it for great good
+* brew healing potions
+* clean up garbage 
+* write helpful messages on the walls
+* make a campfire and invite creatures to it. celebrate together
+* weed your gardens
+* might attract fauna
+
+
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,48 @@ const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 const LIMIT_FPS: i32 = 20;
 
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
+
+const COLOR_DARK_WALL: Color = Color {r: 0, g: 0, b: 100};
+const COLOR_DARK_GROUND: Color = Color {r: 50, g: 50, b: 150};
+
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocking: bool,
+    blocking_sight: bool,
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile {blocking: false, blocking_sight: false,}
+    }
+
+    pub fn wall() -> Self {
+        Tile {blocking: true, blocking_sight: true,}
+    }
+
+}
+
+type Map = Vec<Vec<Tile>>;
+struct Game {
+    map: Map,
+}
+
+fn make_map() -> Map {
+    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+    map[30][22] = Tile::wall();
+    map[50][22] = Tile::wall();
+    map
+}
+
+
 struct Tcod {
     root: Root,
     console: Offscreen,
 }
 
+#[derive(Debug)]
 struct Entity {
     x: i32,
     y: i32,
@@ -22,9 +59,11 @@ impl Entity {
         Entity {x, y, char, color}
     }
 
-    pub fn move_by(&mut self, dx: i32, dy: i32) {
-        self.x += dx;
-        self.y += dy;
+    pub fn move_by(&mut self, dx: i32, dy: i32, game: &Game) {
+        if !game.map[(self.x + dx) as usize][(self.y + dy) as usize].blocking {
+            self.x += dx;
+            self.y += dy;
+        }
     }
 
     pub fn draw(&self, console: &mut dyn Console) {
@@ -41,9 +80,12 @@ fn main() {
         .size(SCREEN_WIDTH, SCREEN_HEIGHT)
         .title("a n t i - r o g u e")
         .init();
-    let console = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+    let console = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
     let mut tcod = Tcod{root, console};
 
+    let game = Game {
+        map: make_map(),
+    };
     let player = Entity::new(SCREEN_WIDTH / 3, SCREEN_HEIGHT / 3, '@', WHITE);
     let npc = Entity::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 'H', YELLOW);
     let mut entities = vec![player, npc];
@@ -51,24 +93,11 @@ fn main() {
     while !tcod.root.window_closed() {
         tcod.console.clear();
 
-        for entity in &entities {
-            entity.draw(&mut tcod.console);
-        }
-
-        blit(
-            &tcod.console,
-            (0, 0),
-            (SCREEN_WIDTH, SCREEN_HEIGHT),
-            &mut tcod.root,
-            (0, 0),
-            1.0,
-            1.0,
-        );
-
+        render_all(&mut tcod, &game, &entities);
 
         tcod.root.flush();
         let player = &mut entities[0];
-        let exit = handle_keys(&mut tcod, player);
+        let exit = handle_keys(&mut tcod, player, &game);
         if exit {
             break;
         }
@@ -76,15 +105,41 @@ fn main() {
 
 }
 
-fn handle_keys(tcod: &mut Tcod, player: &mut Entity) -> bool {
+fn render_all(tcod: &mut Tcod, game: &Game, entities: &[Entity]) {
+    for entity in entities {
+        entity.draw(&mut tcod.console);
+    }
+    entities[0].draw(&mut tcod.console); // drawing player at the end
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let wall = game.map[x as usize][y as usize].blocking_sight;
+            if wall {
+                tcod.console.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+            } else {
+                tcod.console.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            }
+        }
+    }
+    blit(
+            &tcod.console,
+            (0, 0),
+            (MAP_WIDTH, MAP_HEIGHT),
+            &mut tcod.root,
+            (0, 0),
+            1.0,
+            1.0,
+        );
+}
+
+fn handle_keys(tcod: &mut Tcod, player: &mut Entity, game: &Game) -> bool {
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
     let key = tcod.root.wait_for_keypress(true);
     match key {
-        Key {code: Char, printable: 'w', ..} => player.move_by(0, -1),
-        Key {code: Char, printable: 's', ..} => player.move_by(0, 1),
-        Key {code: Char, printable: 'a', ..} => player.move_by(-1, 0),
-        Key {code: Char, printable: 'd', ..} => player.move_by(1, 0),
+        Key {code: Char, printable: 'w', ..} => player.move_by(0, -1, game),
+        Key {code: Char, printable: 's', ..} => player.move_by(0, 1, game),
+        Key {code: Char, printable: 'a', ..} => player.move_by(-1, 0, game),
+        Key {code: Char, printable: 'd', ..} => player.move_by(1, 0, game),
         Key {code: Escape, ..} => return true, // exit game
         _ => {}
     }
